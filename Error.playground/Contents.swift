92897634dd2cd99a56c402830bc50e83d111a0d7
proslin <linchik@mail.ru>
2022-09-29 10:35:05 +0300

    final refactoring

--- Error.playground/Contents.swift
+++ Error.playground/Contents.swift
@@ -30,6 +30,19 @@ enum KitchenError: Error {
     case outOfStock       /// нет в наличии
     case insufficientPlate(platesNeed: Int) /// недостаточно чистых тарелок, передаем недостаточное количество
     case invalidCookingMethod ///не правильный способ приготовления
+    
+    var getErrorDescription: String {
+        switch self {
+        case .invalidSelection:
+            return "блюда нет в меню"
+        case .outOfStock:
+            return "нет в наличии"
+        case .invalidCookingMethod:
+            return "не правильный способ приготовления"
+        case .insufficientPlate(let platesNeed):
+            return "недостаточно чистых тарелок \(platesNeed)"
+        }
+    }
 }
 
 class Kitchen {
@@ -37,7 +50,7 @@ class Kitchen {
                   "Potato": Dish(product: Product(name: "Potato"), count: 7, coocked: .boiled, platesNeed: 2),
                   "Meat": Dish(product: Product(name: "Meat"), count: 0, coocked: .fried, platesNeed: 3)
     ]
-    var platesCount = 8
+    var platesCount = 1
     
     func makeFood(foodName name: String, coocked: CookingMethod) -> (Product?, KitchenError?) {
         
@@ -75,19 +88,19 @@ let cook4 = kitchen.makeFood(foodName: "Potato", coocked: .boiled)
 if let product = cook1.0 {
     print("Мы выбрали: \(product.name)")
 } else if let error = cook1.1 {
-    print("Произошла ошибка: \(error)") }
+    print("Произошла ошибка: \(error.getErrorDescription)") }
 
 if let product = cook2.0 {
     print("Мы выбрали: \(product.name)")
 } else if let error = cook2.1 {
-    print("Произошла ошибка: \(error)") }
+    print("Произошла ошибка: \(error.getErrorDescription)") }
 
 if let product = cook3.0 {
     print("Мы выбрали: \(product.name)")
 } else if let error = cook3.1 {
-    print("Произошла ошибка: \(error)") }
+    print("Произошла ошибка: \(error.getErrorDescription)") }
 
 if let product = cook4.0 {
     print("Мы выбрали: \(product.name)")
 } else if let error = cook4.1 {
-    print("Произошла ошибка: \(error)") }
+    print("Произошла ошибка: \(error.getErrorDescription)") }
